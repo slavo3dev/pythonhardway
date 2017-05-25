71efc0089abb5571648fda1b9ed42425ae4c4a0e
slavo@mimicom24.com <slavo@mimicom24.com>
2017-05-25 09:00:54 -0400

    adding comments

--- ex4.py
+++ ex4.py
@@ -1,10 +1,18 @@
+# Vaule of the car
 cars = 100
+# How many seats have a car
 space_in_a_car = 4.0
+# Number of Drivers
 drivers = 30
+# Number of passenegers
 passengers = 90
+# Account how many car was not driven 
 cars_not_driven = cars - drivers
+# How many cars are driven
 cars_driven = drivers
+# Capacity of the car 
 carpool_capacity = cars_driven * space_in_a_car
+#average passenger per car 
 average_passengers_per_car = passengers / cars_driven
 
 
