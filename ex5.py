c77311e97cd6ae8723bda240f0e31ec190d646b5
slavo@mimicom24.com <slavo@mimicom24.com>
2017-05-26 20:37:23 -0400

    tring to print something

--- ex5.py
+++ ex5.py
@@ -1,13 +1,14 @@
 my_name = 'Slavo'
 my_age = 35 # not a lie
 my_height = 184 # cm
+one_meter = 100
 my_weight = 89 # kg
 my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Bold'
 
 print ("Let's talk about %s." % my_name)
-print ("He's %d inches tall." % my_height)
+print ("He's %d cm or %d m tall." % my_height, my_height / one_meter)
 print ("He's %d pounds heavy." % my_weight)
 print ("Actually that's not too heavy.")
 print ("He's got %s eyes and %s hair." % (my_eyes, my_hair))
