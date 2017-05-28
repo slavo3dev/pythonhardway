abc16beada5d14fceaa1d04a109eeeb69356d9cb
Slavoljub Popovic <slavo@mimicom24.com>
2017-05-28 18:45:24 -0400

    ex.12

---
+++ ex12.py
@@ -0,0 +1,9 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
+
+    # in python 3.* its only input - python 2.7 row_input
