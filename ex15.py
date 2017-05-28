9f7e9f61d44df20ea4231f6fee8f09a4b9547255
Slavoljub Popovic <slavo@mimicom24.com>
2017-05-28 19:24:57 -0400

    new files

--- ex15.py
+++ ex15.py
@@ -6,8 +6,7 @@ filename = argv [1]
 txt = open(filename)
 
 print ("Here's your file %r:" % filename)
-print txt.read()
-
+print (txt.read())
 print ("Type the filename again: ")
 file_again = input("> ")
 
