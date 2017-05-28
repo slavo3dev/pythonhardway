1f8f09b40dd08395dab0f12fa18390712dc960bd
Slavoljub Popovic <slavo@mimicom24.com>
2017-05-28 19:05:38 -0400

    argv its working

--- ex13.py
+++ ex13.py
@@ -4,6 +4,7 @@ script  = argv[0]
 first  = argv[1]
 second = argv[2]
 third = argv[3]
+
 print ("The script is called: ", script)
 print ("Your first variable is: ", first)
 print ("Your second variable is: ", second)
