f6d9da8110fddcf1f514f535936c7f4bfabfc33a
slavo@mimicom24.com <slavo@mimicom24.com>
2017-06-04 19:27:30 -0400

    adding new files

---
+++ ex16-1.py
@@ -0,0 +1,24 @@
+from sys import argv 
+
+script    = argv
+file_name = argv[1]
+
+print('Please fill app all info We are asking You! We will save your file log')
+print('**********************************************************************')
+print('***************** Opening the file ***********************************')
+
+file = open(file_name, 'w')
+
+full_name = input('Please add your First & Last Name? ')
+dob       = input('Add your date of birt? ')
+age       = input('Please add your current age? ')
+city      = input('Where are you born, please add city? ')
+
+print ("All your info will be saved in file: ", file_name)
+
+file.write(full_name, '/n', dob, '/n', age, '/n', city)
+
+print("Plase check your ", file_name)
+file.close()
+
+
