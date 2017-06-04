6bf7b3025e9d3bba5d4042df7b41f5030a3e6e13
slavo@mimicom24.com <slavo@mimicom24.com>
2017-06-04 19:34:38 -0400

    fixing code reading and witting files

--- ex16-1.py
+++ ex16-1.py
@@ -9,14 +9,18 @@ print('***************** Opening the file ***********************************')
 
 file = open(file_name, 'w')
 
-full_name = input('Please add your First & Last Name? ')
-dob       = input('Add your date of birt? ')
-age       = input('Please add your current age? ')
-city      = input('Where are you born, please add city? ')
+print "Truncating the file.  Goodbye!"
+file.truncate()
+print ("I will ask you few questions.")
+
+#full_name = input('Please add your First & Last Name? ')
+dob  = input('Add your date of birt? ')
+age  = input('Please add your current age? ')
+city = input('Where are you born, please add city? ')
 
 print ("All your info will be saved in file: ", file_name)
 
-file.write(full_name, '/n', dob, '/n', age, '/n', city)
+file.write(full_name, '/n',dob, '/n', age, '/n', city)
 
 print("Plase check your ", file_name)
 file.close()
