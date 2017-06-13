747b1c593ad0171c3b08e63e4e42a8de35fb1025
Slavoljub Popovic <slavo@mimicom24.com>
2017-06-12 21:45:42 -0400

    testing

--- ex16-1.py
+++ ex16-1.py
@@ -13,14 +13,17 @@ print ("Truncating the file.  Goodbye!")
 file.truncate()
 print ("I will ask you few questions.")
 
-#full_name = input('Please add your First & Last Name? ')
+full_name = input('Please add your First & Last Name? ')
 dob  = input('Add your date of birt? ')
 age  = input('Please add your current age? ')
 city = input('Where are you born, please add city? ')
 
 print ("All your info will be saved in file: ", file_name)
 
-file.write(full_name, '/n',dob, '/n', age, '/n', city)
+file.write(full_name)
+file.write(dob)
+file.write(age)
+file.write(city)
 
 print("Plase check your ", file_name)
 file.close()
